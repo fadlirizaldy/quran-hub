fa1763828ab510afe7d2f48bc4abbeb08cc163b1
Fadli Rizaldy <85619226+fadlirizaldy@users.noreply.github.com>
2024-07-05 09:43:17 +0700

    feat: add api fetch data tafsir

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,4 +1,4 @@
-import { IDataSurah } from "./api.interface";
+import { IDataSurah, IDataTafsir } from "./api.interface";
 
 const BASE_URL = "https://equran.id/api";
 export async function getAllSurah(): Promise<IDataSurah[]> {
@@ -12,6 +12,15 @@ export async function getAllSurah(): Promise<IDataSurah[]> {
 
 export async function getDetailSurah(id: string): Promise<IDataSurah> {
   const res = await fetch(`${BASE_URL}/surat/${id}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export async function getDetailTafsir(id: string): Promise<IDataTafsir> {
+  const res = await fetch(`${BASE_URL}/tafsir/${id}`);
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }

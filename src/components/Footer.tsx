54e725370156ce62f6cfc3e13dec5df3617f1f03
Fadli Rizaldy <fadly.rizaldy@ai.astra.co.id>
2024-12-29 19:59:26 +0700

    fix(footer): add designed by valid author

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="text-center text-slate-500 text-sm pb-4">
-      &copy; 2024 Fadli Rizaldy. All rights reserved.
+      &copy; 2024 Fadli Rizaldy. Designed by Lafidz Tafara.
     </footer>
   );
 };
